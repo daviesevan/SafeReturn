aa29835fd16f6429bac6dd6a243bbc7388c67ae8
Davies Evan <evandavies739@gmail.com>
2024-07-24 12:43:52 +0300

    Fixed axios interceptor bug

--- frontend/src/api/interceptor.jsx
+++ frontend/src/api/interceptor.jsx
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.VITE_BASE_URL;
+const baseURL = import.meta.env.VITE_BASE_URL;
 
-const api = axios.create({ baseUrl });
-const refreshapi = axios.create({ baseUrl });
+const api = axios.create({
+  baseURL,
+});
+
+const refreshapi = axios.create({
+  baseURL,
+});
+
 
 api.interceptors.request.use(
   (config) => {
